3e5c55492645c67bff845428ba60edbea653d2bf
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2025-12-17 22:14:43 +0530

    Add Azure Data Lake Gen2 filesystem resource to Synapse configuration

--- synapse/main.tf
+++ synapse/main.tf
@@ -1,3 +1,9 @@
+resource "azurerm_storage_data_lake_gen2_filesystem" "datalake_filesystem" {
+  name               = "datalakefilesystem${var.application_name}"
+  storage_account_id = var.storage_account_id
+}
+
+
 resource "azurerm_synapse_workspace" "synapse_workspace" {
   name                                 = "synapseworkspace${var.application_name}"
   resource_group_name                  = var.resource_group_name
