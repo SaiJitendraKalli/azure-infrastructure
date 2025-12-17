6fed7dd115f29e64b5475f8d9af32faa4d619699
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2025-12-17 22:26:02 +0530

    Fix storage_data_lake_gen2_filesystem_id reference in Synapse workspace configuration

--- synapse/main.tf
+++ synapse/main.tf
@@ -1,3 +1,5 @@
+
+
 resource "azurerm_storage_data_lake_gen2_filesystem" "datalake_filesystem" {
   name               = "datalakefilesystem${var.application_name}"
   storage_account_id = var.storage_account_id
@@ -8,7 +10,7 @@ resource "azurerm_synapse_workspace" "synapse_workspace" {
   name                                 = "synapseworkspace${var.application_name}"
   resource_group_name                  = var.resource_group_name
   location                             = var.location
-  storage_data_lake_gen2_filesystem_id = var.storage_account_id
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.datalake_filesystem.id
   sql_administrator_login              = "sqladminuser"
   sql_administrator_login_password     = var.sql_password_secret_id
   identity {
