5c706b7ddb4a0bdee4b05bf421693dc5dba28e89
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2025-12-17 23:59:55 +0530

    Refactor Synapse module to enable Azure AD authentication and comment out unused SQL password variable

--- main.tf
+++ main.tf
@@ -57,14 +57,12 @@ module "databricks" {
 }
 
 module "synapse" {
-  source                 = "./synapse"
-  application_name       = var.application_name
-  location               = var.location
-  resource_group_name    = module.resource_group.resource_group_name
-  sql_password_secret_id = module.key_vault.sql_password_secret_id
-  storage_account_id     = module.storage_account.storage_account_id
-  
-  depends_on             = [module.storage_account, module.key_vault]
+  source              = "./synapse"
+  application_name    = var.application_name
+  location            = var.location
+  resource_group_name = module.resource_group.resource_group_name
+  storage_account_id  = module.storage_account.storage_account_id
+  depends_on          = [module.storage_account, module.key_vault]
 
 }
 
