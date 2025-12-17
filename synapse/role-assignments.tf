d2aa8bf067d003faa54d75b1a7a334b602fef4ec
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2025-12-18 00:47:08 +0530

    Update principal_id in Synapse role assignment to use a specific object ID

--- synapse/role-assignments.tf
+++ synapse/role-assignments.tf
@@ -1,5 +1,5 @@
 resource "azurerm_synapse_role_assignment" "synapse_admin" {
   synapse_workspace_id = azurerm_synapse_workspace.synapse_workspace.id
   role_name            = "Synapse Administrator"
-  principal_id         = data.azurerm_client_config.current.object_id
+  principal_id         = "5aa1aa7a-08a6-4e98-b77d-295f6900fd8f"
 }
