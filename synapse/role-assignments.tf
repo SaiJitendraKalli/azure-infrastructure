fc116b84809ab6bb84b7f2e876c36a9d93eeeec4
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2025-12-18 00:45:21 +0530

    Refactor role assignments to use azurerm_synapse_role_assignment and remove unused azurerm_role_assignment resources

--- synapse/role-assignments.tf
+++ synapse/role-assignments.tf
@@ -1,16 +1,5 @@
-
-resource "azurerm_role_assignment" "synapse_contributor" {
-  scope                = azurerm_synapse_workspace.synapse_workspace.id
-  role_definition_name = "Synapse Administrator"
+resource "azurerm_synapse_role_assignment" "synapse_admin" {
+  synapse_workspace_id = azurerm_synapse_workspace.synapse_workspace.id
+  role_name            = "Synapse Administrator"
   principal_id         = data.azurerm_client_config.current.object_id
-  depends_on           = [azurerm_synapse_firewall_rule.allow_azure_services]
-
-}
-
-resource "azurerm_role_assignment" "user_role" {
-  scope                = azurerm_synapse_workspace.synapse_workspace.id
-  role_definition_name = "Synapse Administrator"
-  principal_id         = "5aa1aa7a-08a6-4e98-b77d-295f6900fd8f"
-  depends_on           = [azurerm_synapse_firewall_rule.allow_azure_services]
-
 }
