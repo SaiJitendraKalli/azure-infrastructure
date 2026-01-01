26b1910cb164262e839f0d9f14be35b5bed5de85
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2026-01-01 14:24:26 +0530

    fix: format data block for azuread_service_principal and add azurerm_client_config

--- function-app/data.tf
+++ function-app/data.tf
@@ -1,3 +1,6 @@
 data "azuread_service_principal" "service_principal" {
- display_name = var.service_principal_name
+  display_name = var.service_principal_name
+}
+data "azurerm_client_config" "current" {
+
 }
