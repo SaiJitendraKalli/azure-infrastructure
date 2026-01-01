eb99a7662975fa5176cce8142f16770836878ca6
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2026-01-01 14:15:29 +0530

    fix: correct output name from client_id to id in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ data "azurerm_client_config" "current" {
 output "client_id" {
   value = data.azurerm_client_config.current.client_id
 }
-output "client_id" {
+output "id" {
   value = data.azurerm_client_config.current.id
 }
 output "object_id" {
