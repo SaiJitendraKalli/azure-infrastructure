ece9add8a4c97527d9e5270cfbd6b101275794ce
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2026-01-01 15:37:06 +0530

    fix: update storage account name to include environment variable

--- storage-account/main.tf
+++ storage-account/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "sadatamaester${var.location}"
+  name                     = "sadatamaes${var.location}${var.environment}"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
